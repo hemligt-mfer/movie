1f58098099a99bd31eb7e8166effda2424b7e896
Adam Lundvall <adam.lundvall@gmail.com>
2026-04-23 13:38:23 +0200

    Changed the page.tsx testing

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,55 +1,7 @@
 export default function Home() {
-<<<<<<< HEAD
-  return (
-    <div>
-      <h1>Testing 1 2 3 1 2 3</h1>
-      <p>
-        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Illo pariatur
-        quisquam nobis voluptates ut magnam eum in officiis quidem. Sapiente
-        temporibus sunt tenetur dolores eveniet beatae, necessitatibus
-        asperiores tempore rerum, inventore magni minus quisquam? Saepe earum,
-        deserunt possimus distinctio nulla facilis quisquam a nihil veritatis
-        nesciunt ab excepturi voluptates laborum omnis iusto pariatur quia illum
-        doloribus ut! Ducimus incidunt nisi quasi eos iure voluptates obcaecati
-        odio, repellat iste mollitia, earum laudantium maiores recusandae ex!
-        Reiciendis fuga fugiat ratione soluta! Sint illo praesentium debitis
-        expedita odit commodi enim pariatur aut? Optio sapiente aspernatur
-        facilis maiores, minus sunt repellat vel exercitationem ipsa?
-      </p>
-      <p>
-        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Illo pariatur
-        quisquam nobis voluptates ut magnam eum in officiis quidem. Sapiente
-        temporibus sunt tenetur dolores eveniet beatae, necessitatibus
-        asperiores tempore rerum, inventore magni minus quisquam? Saepe earum,
-        deserunt possimus distinctio nulla facilis quisquam a nihil veritatis
-        nesciunt ab excepturi voluptates laborum omnis iusto pariatur quia illum
-        doloribus ut! Ducimus incidunt nisi quasi eos iure voluptates obcaecati
-        odio, repellat iste mollitia, earum laudantium maiores recusandae ex!
-        Reiciendis fuga fugiat ratione soluta! Sint illo praesentium debitis
-        expedita odit commodi enim pariatur aut? Optio sapiente aspernatur
-        facilis maiores, minus sunt repellat vel exercitationem ipsa?
-      </p>
-      <p>
-        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Illo pariatur
-        quisquam nobis voluptates ut magnam eum in officiis quidem. Sapiente
-        temporibus sunt tenetur dolores eveniet beatae, necessitatibus
-        asperiores tempore rerum, inventore magni minus quisquam? Saepe earum,
-        deserunt possimus distinctio nulla facilis quisquam a nihil veritatis
-        nesciunt ab excepturi voluptates laborum omnis iusto pariatur quia illum
-        doloribus ut! Ducimus incidunt nisi quasi eos iure voluptates obcaecati
-        odio, repellat iste mollitia, earum laudantium maiores recusandae ex!
-        Reiciendis fuga fugiat ratione soluta! Sint illo praesentium debitis
-        expedita odit commodi enim pariatur aut? Optio sapiente aspernatur
-        facilis maiores, minus sunt repellat vel exercitationem ipsa?
-      </p>
-    </div>
-  );
-=======
     return (
         <div>
-            <h1>Tesating 1 2 3 1 2 3</h1>
-            <p>Hello</p>
+            <h1>ejinfrekom</h1>
         </div>
-    )
->>>>>>> 6818fdc3790753b87fcb31d81eb1ca1e32050cf3
+    );
 }
